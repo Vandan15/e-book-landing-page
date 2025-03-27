420e3b4351cd9b06f9c24eb91ed6008a4188bf87
vandan-khamkar-us <vandan.khamkar@logicwind.com>
2025-03-27 19:31:22 +0530

    update price

--- app/components/PurchaseModal.tsx
+++ app/components/PurchaseModal.tsx
@@ -62,7 +62,7 @@ const PurchaseModal = ({
           button_location: "inside_purchase_modal",
           ecommerce: {
             currency: "INR",
-            value: 199,
+            value: 299,
           },
         });
         setStep(2);
@@ -101,7 +101,7 @@ const PurchaseModal = ({
           button_location: "inside_purchase_modal",
           ecommerce: {
             currency: "INR",
-            value: 199,
+            value: 299,
           },
         });
         setStep(3);
